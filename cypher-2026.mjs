6d7613e7f7a0ac48d82d2b8b45c56d07407a93d3
uzury <29405754+uzury@users.noreply.github.com>
2026-08-22 00:13:08 -0300

    fix: register lasting damage lifecycle

--- cypher-2026.mjs
+++ cypher-2026.mjs
@@ -1,41 +1,42 @@
 import { registerDataModels } from "./scripts/bootstrap/register-data-models.mjs";
-  import { registerSheets } from "./scripts/bootstrap/register-sheets.mjs";
-  import { registerHandlebars } from "./scripts/bootstrap/register-handlebars.mjs";
+import { registerSheets } from "./scripts/bootstrap/register-sheets.mjs";
+import { registerHandlebars } from "./scripts/bootstrap/register-handlebars.mjs";
+import { registerLastingDamageLifecycle } from "./scripts/domain/wounds/lasting-damage-lifecycle.mjs";
 
-  Hooks.once("init", () => {
-    CONFIG.Item ??= {};
-    CONFIG.Item.typeLabels ??= {};
-    CONFIG.Item.typeLabels.characterArc ??= "Character Arc";
+Hooks.once("init", () => {
+  CONFIG.Item ??= {};
+  CONFIG.Item.typeLabels ??= {};
+  CONFIG.Item.typeLabels.characterArc ??= "Character Arc";
 
-    CONFIG.Item.documentClassMap ??= {};
-    CONFIG.Item.documentClassMap.characterArc ??= Item;
+  CONFIG.Item.documentClassMap ??= {};
+  CONFIG.Item.documentClassMap.characterArc ??= Item;
 
-    const itemTypeMap = (game.system?.documentTypes?.Item && typeof game.system.documentTypes.Item === "object") ? game.system.documentTypes.Item : {};
-    if (game.system) {
-      game.system.documentTypes ??= {};
-      game.system.documentTypes.Item = {
-        ...itemTypeMap,
-        characterArc: {}
-      };
-    }
+  const itemTypeMap = (game.system?.documentTypes?.Item && typeof game.system.documentTypes.Item === "object") ? game.system.documentTypes.Item : {};
+  if (game.system) {
+    game.system.documentTypes ??= {};
+    game.system.documentTypes.Item = {
+      ...itemTypeMap,
+      characterArc: {}
+    };
+  }
 
-    registerDataModels();
-    registerSheets();
-    registerHandlebars();
-  });
+  registerDataModels();
+  registerSheets();
+  registerHandlebars();
+  registerLastingDamageLifecycle();
+});
 
-  Hooks.once("ready", () => {
-    CONFIG.Item ??= {};
-    CONFIG.Item.typeLabels ??= {};
-    CONFIG.Item.typeLabels.characterArc ??= "Character Arc";
+Hooks.once("ready", () => {
+  CONFIG.Item ??= {};
+  CONFIG.Item.typeLabels ??= {};
+  CONFIG.Item.typeLabels.characterArc ??= "Character Arc";
 
-    CONFIG.Item.documentClassMap ??= {};
-    CONFIG.Item.documentClassMap.characterArc ??= Item;
+  CONFIG.Item.documentClassMap ??= {};
+  CONFIG.Item.documentClassMap.characterArc ??= Item;
 
-    if (game.system) {
-      game.system.documentTypes ??= {};
-      game.system.documentTypes.Item ??= {};
-      game.system.documentTypes.Item.characterArc ??= {};
-    }
-  });
-  
+  if (game.system) {
+    game.system.documentTypes ??= {};
+    game.system.documentTypes.Item ??= {};
+    game.system.documentTypes.Item.characterArc ??= {};
+  }
+});
